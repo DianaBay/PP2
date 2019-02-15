748dd184f18c64123abad13d1b891d339629027e
DianaBay <47646803+DianaBay@users.noreply.github.com>
2019-02-15 12:29:14 +0600

    Add files via upload

---
+++ Lab1Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;  // amount of input numbers
+            k = int.Parse(Console.ReadLine()); // reading line and converting it to int 
+            int[] arr = new int[k];  // new array with length k
+            string[] numbers = Console.ReadLine().Split(); // reading and spliting
+            for (int i = 0; i < numbers.Length; ++i) // cycle for numbers
+            {
+                for (int j = 0; j < 2; ++j)
+                {
+                    Console.Write(numbers[i] + " "); // duplicate number
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
