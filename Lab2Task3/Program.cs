f4466d968e0c1c8117c3ac49251938f729b9dcbd
DianaBay <47646803+DianaBay@users.noreply.github.com>
2019-02-15 12:33:23 +0600

    Add files via upload

---
+++ Lab2Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace L2T3
+{
+    class Program
+    {
+        static void rec(DirectoryInfo dir, string level)
+        {
+            Console.WriteLine(level + dir.Name);
+            level += "    ";
+            FileSystemInfo[] fs = dir.GetFileSystemInfos();
+            for (int i = 0; i < fs.Length; ++i)
+            {
+                if (fs[i].GetType() == typeof(FileInfo))
+                {
+                    Console.WriteLine(level + fs[i].Name);
+                }
+                else
+                {
+                    rec(fs[i] as DirectoryInfo, level);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Alser\source\repos\Lab2Task1\Lab2Task3");
+            rec(dir, "");
+            Console.ReadKey();
+        }
+    }
+}
