2387cacb4ed6becff412eff65337384f5ced0f9a
DianaBay <47646803+DianaBay@users.noreply.github.com>
2019-02-15 12:34:05 +0600

    Add files via upload

---
+++ Lab2Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace L2T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Alser\source\repos\Lab2Task1\Lab2Task4\Example.txt";
+            string path1 = @"C:\Users\Alser\source\repos\Lab2Task1\Lab2Task4";
+            DirectoryInfo dirInfo = new DirectoryInfo(path1);
+            FileInfo fi = new FileInfo(path);
+            if (fi.Exists && dirInfo.Exists)
+            {
+                fi.CopyTo(path1 + @"\Copied.txt", true);
+                fi.Delete();
+            }
+            else Console.WriteLine("path or path1 not found:(");
+            Console.ReadKey();
+        }
+    }
+}
