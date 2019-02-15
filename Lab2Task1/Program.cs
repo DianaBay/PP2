7e4da609baf8e7f96166cfcd05f2396d764c89bd
DianaBay <47646803+DianaBay@users.noreply.github.com>
2019-02-15 12:31:46 +0600

    Add files via upload

---
+++ Lab2Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace L2T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\Alser\source\repos\Lab2Task1\input.txt");
+            char[] txet = text.ToCharArray();
+            Array.Reverse(txet);
+            string s = new string(txet);
+            if (s == text)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+            Console.ReadKey();
+        }
+    }
+}
